2dff09917d4e8f8e6f35704adfae7538d409f83b
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-11-16 12:15:17 +0100

    Added comments to fileExplorer and memoryAnalysis

--- fileExplorer.js
+++ fileExplorer.js
@@ -1,15 +1,20 @@
 const fs = require("fs/promises");
 const path = require("path");
 
+// Funkcja do eksploracji systemu plików
 async function exploreFileSystem(directoryPath) {
   try {
+    // Funkcja rekurencyjna do eksploracji katalogu
     async function exploreDirectory(currentPath) {
+      // Pobieranie listy plików w bieżącym katalogu
       const files = await fs.readdir(currentPath);
 
       for (const file of files) {
         const filePath = path.join(currentPath, file);
+        // Pobieranie informacji o pliku
         const stats = await fs.stat(filePath);
 
+        // Sprawdzanie, czy to katalog czy plik
         if (stats.isDirectory()) {
           console.log(`[Folder] ${filePath}`);
           await exploreDirectory(filePath);
@@ -28,5 +33,5 @@ async function exploreFileSystem(directoryPath) {
     console.error(`Error exploring the file system: ${error.message}`);
   }
 }
-
+// Wywołanie funkcji eksplorującej system plików z bieżącego katalogu
 exploreFileSystem(__dirname);
--- memoryAnalysis.js
+++ memoryAnalysis.js
@@ -1,10 +1,13 @@
 const os = require("os");
 
+// Funkcja memoryAnalysis: Analizuje pamięć systemową i wirtualną oraz wyświetla wyniki.
 function memoryAnalysis() {
+  // Pobieranie informacji o pamięci systemowej
   const totalMemory = os.totalmem();
   const freeMemory = os.freemem();
   const usedMemory = totalMemory - freeMemory;
 
+  // Pobieranie informacji o pamięci wirtualnej
   const virtualMemory = os.totalmem() - os.freemem();
   const usedVirtualMemory = os.totalmem() - os.freemem() - os.freemem();
 
@@ -16,7 +19,7 @@ function memoryAnalysis() {
   console.log(`Virtual Memory: ${formatBytes(virtualMemory)}`);
   console.log(`Used Virtual Memory: ${formatBytes(usedVirtualMemory)}`);
 }
-
+// Funkcja formatBytes: Konwertuje ilość bajtów na odpowiednią jednostkę (KB, MB, GB).
 function formatBytes(bytes) {
   const kibibytes = bytes / 1024;
   const mebibytes = kibibytes / 1024;
