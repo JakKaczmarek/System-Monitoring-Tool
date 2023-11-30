050c4dce6b261b50272f2970e360998d1eac833e
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-11-30 12:37:58 +0100

    Fixed monitorFolder.js

--- monitorFolder.js
+++ monitorFolder.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
 
+// Ścieżka do monitorowania zmian w katalogu
 const directoryToWatch = "D:/Users/Kuba/system-monitoring-tool/";
 
+// Uruchamianie monitora zmian w katalogu
 fs.watch(directoryToWatch, (eventType, filename) => {
+  // Sprawdzanie typu zdarzenia (zmiana, przeniesienie, usunięcie)
   if (eventType === "change") {
     console.log(`File ${filename} has been modified.`);
   } else if (eventType === "rename") {
+    // Sprawdzanie, czy plik istnieje po zdarzeniu "rename"
     fs.exists(directoryToWatch + filename, (exists) => {
       if (exists) {
         console.log(`File ${filename} has been added.`);
