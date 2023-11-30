d89864260848b98347594ae32d36c534689a1fc1
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-11-30 12:48:13 +0100

    Added comments to files

--- loggedUser.js
+++ loggedUser.js
@@ -1,18 +1,26 @@
 const { exec } = require("child_process");
 
+// Funkcja monitorująca zalogowanych użytkowników
 function monitorLoggedUsers() {
+  // Pobieranie bieżącej platformy (systemu operacyjnego)
   const platform = process.platform;
   let command;
 
+  // Sprawdzenie, czy platforma to Windows
   if (platform === "win32") {
+    // Polecenie do pobierania aktualnie zalogowanego użytkownika w systemie Windows
+
     command = "whoami";
   } else {
     console.error("Unsupported operating system");
     return;
   }
 
+  // Wykonywanie polecenia i określanie kodowania jako UTF-8
+
   exec(command, { encoding: "utf-8" }, (err, username, stderr) => {
     if (err) {
+      // Obsługa błędów podczas wykonywania polecenia
       console.error(`Error executing command: ${stderr}`);
       return;
     }
--- monitorProcesses.js
+++ monitorProcesses.js
@@ -2,9 +2,11 @@ const { exec } = require("child_process");
 const pidusage = require("pidusage");
 const iconv = require("iconv-lite");
 
+// Funkcja pobierająca informacje o procesach i użyciu pamięci
 function getProcessesAndMemoryUsage() {
   const tasklistCommand = "tasklist /fo csv";
 
+  // Wykonywanie polecenia 'tasklist' i określanie kodowania jako bufor
   exec(
     tasklistCommand,
     { encoding: "buffer" },
@@ -13,13 +15,14 @@ function getProcessesAndMemoryUsage() {
         console.error(`Error in tasklist command: ${stderr}`);
         return;
       }
-
+      // Dekodowanie wyników polecenia 'tasklist' z kodowania cp1250
       const decodedTasklistOutput = iconv.decode(tasklistOutput, "cp1250");
 
       const tasklistLines = decodedTasklistOutput
         .split("\r\n")
         .map((line) => line.split('","'));
 
+      // Iteracja przez pierwsze 15 linii (z pominięciem nagłówka)
       for (
         let index = 1;
         index < tasklistLines.length && index <= 15;
@@ -31,6 +34,7 @@ function getProcessesAndMemoryUsage() {
         const cpuTime = process[4].replace('"', "");
 
         try {
+          // Pobieranie statystyk użycia zasobów dla danego procesu
           const processStats = await pidusage(processId);
           console.log(
             `Process Name: ${processName}, Process ID: ${processId}, CPU Time: ${cpuTime}, Memory Usage: ${JSON.stringify(
@@ -44,6 +48,7 @@ function getProcessesAndMemoryUsage() {
         }
       }
 
+      // Oczekiwanie 5 sekund przed zakończeniem programu (funkcja setTimeout bezczynna)
       setTimeout(() => {}, 5000);
     }
   );
